6f4238e9a0c11b4849fdad39d844cf357433f09a
Elias Annila <eannila@kesa4.x.csc.fi>
2017-06-22 07:58:49 +0300

    latailu lisätty

--- atom_reader.py
+++ atom_reader.py
@@ -1,10 +1,10 @@
-import feedparser
 import xml.etree.ElementTree as ET
 import os
 import urllib
 from subprocess import call
 from sets import Set
 from datetime import datetime
+
 
 def add_params(url, params):
 	if params:
@@ -26,37 +26,41 @@ def check_local_files(rootdir, feed_sets, removed_set):
 		for file in files:
 			fn = os.path.join(subdir, file)
 			if not is_in_sets(feed_sets, fn):
-				removed_set[fn] = ''
-	
+				removed_set[fn] = [None, None,None,None,None]
+				update_local_time_and_size(fn,removed_set[fn])
+						
 def print_set(header, st):
 	print ''
 	print str(len(st))+' '+ header
 	for f in st:
-		print '	', f	
+		print '	', f, st[f][1],st[f][2],st[f][3],st[f][4]	
 
 
 def print_report(headers, sets):
 	for st, hd in zip(sets, headers):
 		print_set(hd,st)
 
-def check_newer(local, dl_link, update_set, aof_set, conflict_set, matching_set):
+
+
+def download_set(a_set):
+	for link in a_set:
+		call(['wget', '-O', link,a_set[link][0]]) 
+
+def update_local_time_and_size(local, meta):
+	if os.path.exists(local):
+		mtime = os.path.getmtime(local)
+		meta[1] = datetime.utcfromtimestamp(mtime)
+		meta[3] = os.path.getsize(local)	
+
+
+def update_time_and_size(local, meta):
+	update_local_time_and_size(local, meta)
 	
-	mtime = os.path.getmtime(local)
-	local_time = datetime.utcfromtimestamp(mtime)
-	local_size = os.path.getsize(local)	
-	
-	url_meta = urllib.urlopen(dl_link).info()
+	url_meta = urllib.urlopen(meta[0]).info()
 	feed_time = datetime.strptime(url_meta.getheader('Last-Modified'), '%a, %d %b %Y %H:%M:%S %Z')
 	feed_size = int(url_meta.getheader('Content-Length'))
-
-	if local_time == feed_time and local_size == feed_size:
-		matching_set[local] = dl_link
-	elif local_time < feed_time:
-		update_set[local] = dl_link
-	elif feed_time < local_time:
-		aof_set[local] = dl_link
-	else:
-		conflict_set[local] = dl_link
+	meta[2]=feed_time
+	meta[4]=feed_size
 
 def write_file_from_dict(d, file_name):	
 	with open(file_name, 'w') as f:
@@ -72,22 +76,11 @@ def get_dataset(link, params, base_url, base_folder):
 	print s
 	print entry_feed.find(ns+'title').text
 	
-	dl_file = 'dl_list.txt'	
 	dl_set = {}
-	
-	update_file = 'update_list.txt'	
 	update_set = {}
-
-	aof_file = 'ahead_of_feed_list.txt' #Ahead Of Feed	
 	aof_set = {}
-
-	conflict_file = 'conflict_list.txt' #matching timestamps, but different file sizes	
 	conflict_set = {}
-
-	removed_file = 'removed_list.txt'	
 	removed_set = {}
-
-	removed_file = 'matching_list.txt'	
 	matching_set = {}
 
 		
@@ -95,34 +88,61 @@ def get_dataset(link, params, base_url, base_folder):
 		dl_link = child.find(ns+'link').get('href')		
 		path,slash, f = dl_link.split('?')[0][len(base_url):].rpartition(r'/')
 		path = base_folder + '/' + path		
-		filename = path + slash + f				 		
+		filename = path + slash + f
+
+		meta = [dl_link,None,None,None,None]
+		update_time_and_size(filename, meta)
+			 		
 		if not os.path.exists(path):
-			os.makedirs(path)
-	
+			os.makedirs(path)		
 		if not os.path.exists(filename):				
-			dl_set[filename] = dl_link
+			dl_set[filename] = meta
 		elif os.path.isdir(filename):
-			print 'Trying to overwrite folder with file. Something is definately wrong here...'
+			print 'Trying to overwrite a folder with a file. Something is probably wrong here.'
 		else:
-			check_newer(filename, dl_link, update_set, aof_set, conflict_set, matching_set)
+
+			if meta[1] == meta[2] and meta[3] == meta[4]:
+				matching_set[filename] = meta
+			elif meta[1] < meta[2]:
+				update_set[filename] = meta
+			elif meta[1] > meta[2]:
+				aof_set[filename] = meta
+			else:
+				conflict_set[filename] = meta
 	
 	check_local_files(base_folder, [update_set, aof_set, conflict_set, dl_set, matching_set], removed_set)	
+
+	return dl_set, update_set, aof_set, conflict_set, removed_set, matching_set
+
+
+def write_sets_to_files(dl_set, update_set, aof_set, conflict_set, removed_set, matching_set):
+	dl_file = 'dl_list.txt'	
+	update_file = 'update_list.txt'	
+	aof_file = 'ahead_of_feed_list.txt' #Ahead Of Feed	
+	conflict_file = 'conflict_list.txt' #matching timestamps, but different file sizes
+	removed_file = 'removed_list.txt'	
+	matching_file = 'matching_list.txt'	
+	
+
 	write_file_from_dict(dl_set,dl_file)
 	write_file_from_dict(update_set,update_file)
 	write_file_from_dict(aof_set,aof_file)
 	write_file_from_dict(conflict_set,conflict_file)
 	write_file_from_dict(removed_set,removed_file)
-
-
-	print_report(['new files to be downloaded', 'files to be updated', 'files with newer local versions', 'files with matching timestamps, but conflicting sizes', 'files not found in feed', 'files matching those in feed'], [dl_set, update_set, aof_set, conflict_set, removed_set, matching_set])	
 	
+	
+
+sets = get_dataset('http://wwwd3.ymparisto.fi/d3/atom/luonnonsuojelualueet.xml',None,'http://wwwd3.ymparisto.fi/d3/', 'syke')
+print_report(['new files to be downloaded', 'files to be updated', 'files with newer local versions', 'files with matching timestamps, but conflicting sizes', 'files not found in feed', 'files matching those in feed'], sets)	
+
+download_set(sets[0])
 
 
 #get_dataset('https://tiedostopalvelu.maanmittauslaitos.fi/tp/feed/mtp/laser/etrs-tm35fin-n2000',['api_key=bg5gt3qcn0gkv04rbukeam1btb', 'updated=2017-03-27T00:00'],'https://tiedostopalvelu.maanmittauslaitos.fi/tp/tilauslataus/tuotteet/', 'mml', 3)
 
 
-get_dataset('http://wwwd3.ymparisto.fi/d3/atom/luonnonsuojelualueet.xml',None,'http://wwwd3.ymparisto.fi/d3/', 'syke')
-
 
 print "\nAll Done"
+
+
 
